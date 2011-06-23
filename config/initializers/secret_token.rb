467b66dc53bcc0ee915d5bb06d5ff8de32d55260
mpa <mpa@iron.local>
2011-06-23 19:39:46 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Refinerycms::Application.config.secret_token = 'cbe3d635a21d8fb3a71a7aed5105fa81ceb9d2de0fdbdd31aa502ae7e6cdc27606456da163a62b05bd08e406b911f4e87da575e2da925c5099886ccea16cf7c8'
